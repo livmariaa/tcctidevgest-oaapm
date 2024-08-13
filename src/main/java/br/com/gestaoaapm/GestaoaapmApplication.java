ca0af189f368849e068821baca77c01ee8036456
emilybeatriz541@gmail.com <emilybeatriz541@gmail.com>
2024-08-13 08:31:12 -0300

    Commit da estrutura inicial do projeto TCC

---
+++ src/main/java/br/com/gestaoaapm/GestaoaapmApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaoaapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoaapmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoaapmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaoaapm/GestaoaapmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoaapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoaapmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
