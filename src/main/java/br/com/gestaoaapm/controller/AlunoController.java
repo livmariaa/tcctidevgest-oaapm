ec21b07fafd18809d53b0d77a449b51e9454d5c0
CT-DEV <liviamariameira30@gmail.com>
2024-08-20 08:12:52 -0300

    adicionar novos arquivos

--- src/main/java/br/com/gestaoaapm/controller/AlunoController.java
+++ src/main/java/br/com/gestaoaapm/controller/AlunoController.java
@@ -1,18 +1,94 @@
 package br.com.gestaoaapm.controller;
 
+import br.com.gestaoaapm.model.Aluno;
+import br.com.gestaoaapm.repository.AlunoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/aluno")
 public class AlunoController {
 
-    @GetMapping
-    public String Index(Model model){
+    @Autowired
+    private AlunoRepository alunoRepository;
 
+    @GetMapping
+    public String Index(Model model) {
+        model.addAttribute("alunos", alunoRepository.findAll());
         return "aluno/index";
 
     }
+
+    // Carrega o form-inserir.html
+    @GetMapping("/form-inserir")
+    public String formInserir(Model model) {
+
+        model.addAttribute("aluno", new Aluno());
+        return "aluno/form-inserir";
+    }
+
+    // Método para salvar o jogador
+    @PostMapping("/inserir")
+    public String inserir(
+            @Valid Aluno aluno,
+            BindingResult result,
+            RedirectAttributes redirectAttributes) {
+
+        // Verifica se há erros de validação
+        if (result.hasErrors()) {
+            return "aluno/form-inserir";
+        }
+
+        alunoRepository.save(aluno);
+        redirectAttributes.addFlashAttribute("mensagem", "Aluno salvo com sucesso!");
+        return "redirect:/aluno";
+    }
+
+    // Formulário de Alteração dos Jogadores
+    @GetMapping("/form-alterar/{id}")
+    public String formAlterar(@PathVariable("id") Long id, Model model){
+
+        Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Aluno inválido: " + id));
+
+        model.addAttribute("aluno", aluno);
+        return "aluno/form-alterar";
+    }
+
+    // Método para alterar o jogador
+    @PostMapping("/alterar")
+    public String alterar(
+            @Valid Aluno aluno,
+            BindingResult result,
+            RedirectAttributes redirectAttributes){
+
+        // Verifica se há erros de validação
+        if(result.hasErrors()){
+            return "cadastros/aluno/form-alterar";
+        }
+
+        alunoRepository.save(aluno);
+        redirectAttributes.addFlashAttribute("mensagem", "Aluno alterado com sucesso!");
+        return "redirect:/aluno";
+    }
+
+    // Método para excluir o jogador
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, RedirectAttributes redirectAttributes){
+        Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Aluno inválido: " + id));
+        alunoRepository.delete(aluno);
+        redirectAttributes.addFlashAttribute("mensagem", "Aluno excluído com sucesso!");
+        return "redirect:/aluno";
+    }
+
 }
+
+
+
