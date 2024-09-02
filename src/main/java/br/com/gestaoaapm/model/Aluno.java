de8dc69781b0c633f7456e5e65ca82dfa0e107ff
julliarafaela <julliaraprestes@gmail.com>
2024-09-02 13:00:51 -0300

    Criaçao das rotinas de validaçoes dos campos dos formularios

--- src/main/java/br/com/gestaoaapm/model/Aluno.java
+++ src/main/java/br/com/gestaoaapm/model/Aluno.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Data
 @AllArgsConstructor
@@ -17,9 +22,14 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "O nome deve ser preenchido")
     private String nome;
     private String telefone;
+    @NotEmpty(message = "O email deve ser preenchido")
+    @Email(message = "O email deve ser valido")
     private String email;
+
+    @CPF(message = "O cpf deve ser valido")
     private String cpf;
     private String endereco;
     private String turma;
