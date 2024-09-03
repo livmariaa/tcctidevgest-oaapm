b29c6666e3a68f4876fde7e6b0c93122428ecef1
emilybeatriz541@gmail.com <emilybeatriz541@gmail.com>
2024-09-03 07:48:18 -0300

    Add FuncionarioRepository :)

--- src/main/java/br/com/gestaoaapm/repository/FuncionarioRepository.java
+++ src/main/java/br/com/gestaoaapm/repository/FuncionarioRepository.java
@@ -1,4 +1,36 @@
 package br.com.gestaoaapm.repository;
 
-public interface FuncionarioRepository {
+import br.com.gestaoaapm.model.Aluno;
+import br.com.gestaoaapm.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    List<Funcionario> findByNomeContainingIgnoreCase(String nome);
+
+    Funcionario findByEmail(String email);
+
+    Funcionario findByCpf(String cpf);
+
+    Funcionario findBycargo(String cargo);
+
+    Optional<Funcionario> findByEmailOrCpf(String email, String cpf);
+
+
+    // Pesquisa o email e o id seja diferente
+    // do id que está sendo alterado
+    Funcionario findByEmailAndIdNot(String email, Long id);
+
+    // Pesquisa o cpf e o id seja diferente
+    // do id que está sendo alterado
+    Funcionario findByCpfAndIdNot(String cpf, Long id);
+
+
+    /*//Pesquisa todos os Funcionarios e retona o id, nome, matricula e cpf
+    @Query("select id, nome, cpf, email, enderecoPessoa.id from Funcionarios")
+    List<Object[]> buscarFuncionariosParaApp();*/
 }
+
