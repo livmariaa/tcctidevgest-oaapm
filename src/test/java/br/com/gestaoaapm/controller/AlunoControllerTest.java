72c2e33871d5f2f03feb02d6eea24c2fdf5f79f0
CT-DEV <liviamariameira30@gmail.com>
2024-09-09 13:42:55 -0300

    add conexões

---
+++ src/test/java/br/com/gestaoaapm/controller/AlunoControllerTest.java
@@ -0,0 +1,32 @@
+package br.com.gestaoaapm.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlunoControllerTest {
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void formInserir() {
+    }
+
+    @Test
+    void salvar() {
+    }
+
+    @Test
+    void formAlterar() {
+    }
+
+    @Test
+    void alterar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+}
