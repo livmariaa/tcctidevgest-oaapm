97c57c869f23757e1ce5ef3086e19a2619bc3ab1
CT-DEV <liviamariameira30@gmail.com>
2024-11-05 16:06:25 -0300

    add valor do produto

--- src/main/java/br/com/gestaoaapm/model/Produto.java
+++ src/main/java/br/com/gestaoaapm/model/Produto.java
@@ -6,25 +6,22 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import org.springframework.format.annotation.NumberFormat;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Produto {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotEmpty(message = "O nome deve ser preenchido")
     private String nome;
+
+    @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
     @NotNull(message = "O preço deve ser preenchido")
     private Double preco;
     @NotEmpty(message = "O status deve ser preenchido")
     private String status;
-
-
 }
-
